d65b6f69c97f3907e40d2b2102885388eafc1977
Rikka <rikka@takanashi.moe>
2026-07-25 17:38:41 +0200

    Poprawiono wiele rzeczy związanych z budowaniem i działaniem aplikacji pod Androidem

--- android/app/src/main/java/pl/zupanet/tablica/TablicaActivity.java
+++ android/app/src/main/java/pl/zupanet/tablica/TablicaActivity.java
@@ -7,9 +7,42 @@
 
 package pl.zupanet.tablica;
 
+import android.os.Bundle;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
 import org.libsdl.app.SDLActivity;
 
 public class TablicaActivity extends SDLActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            getWindow().setDecorFitsSystemWindows(false);
+
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if(controller != null) {
+                controller.hide(WindowInsets.Type.systemBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        }
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.systemBars());
+            }
+        }
+    }
 
     @Override
     protected String[] getLibraries() {
--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -2,6 +2,11 @@ set(APP_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
 set(APP_VERSION_MINOR ${PROJECT_VERSION_MINOR})
 set(APP_VERSION_PATCH ${PROJECT_VERSION_PATCH})
 
+math(EXPR APP_VERSION_CODE
+        "${APP_VERSION_MAJOR} * 100 +
+         ${APP_VERSION_MINOR} * 10 +
+         ${APP_VERSION_PATCH}")
+
 set(APP_VERSION_STRING
     "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}"
 )
@@ -11,6 +16,14 @@ if(WIN32)
     configure_file(
         ${CMAKE_SOURCE_DIR}/resources/windows/version.rc.in
         ${CMAKE_BINARY_DIR}/version.rc
+        @ONLY
+    )
+
+elseif(ANDROID)
+
+    configure_file(
+        ${CMAKE_SOURCE_DIR}/android/app/build.gradle.in
+        ${CMAKE_BINARY_DIR}/build.gradle
         @ONLY
     )
 

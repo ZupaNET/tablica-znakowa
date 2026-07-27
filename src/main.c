d5dad6e3526ebfc6ac8c3d5df1532fed414d9ae8
Rikka <rikka@takanashi.moe>
2026-07-27 16:57:04 +0200

    feat(main): add stb_image to load icon from PNG and not BMP

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3_ttf/SDL_ttf.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
 #include "config/config.h"
 #include "display/display.h"
@@ -25,6 +27,39 @@ static void print_header()
     SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION,"       ┃┏┛┃┃┃┣┻┓┣╸ ┃┣━┫      \n");
     SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION,"       ┗┛ ╹ ╹╹ ╹┗━╸╹╹ ╹      \n");
     SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION,"\n");
+}
+
+static SDL_Surface* load_image(const char *filename)
+{
+    int width, height, channels;
+
+    unsigned char *pixels = stbi_load(filename, &width, &height, &channels, STBI_rgb_alpha);
+    if (!pixels)
+        return nullptr;
+
+    SDL_Surface *surface = SDL_CreateSurface(width, height, SDL_PIXELFORMAT_RGBA32);
+    if (!surface)
+    {
+        stbi_image_free(pixels);
+        return nullptr;
+    }
+
+    SDL_LockSurface(surface);
+
+    for (int y = 0; y < height; y++)
+    {
+        SDL_memcpy(
+            (Uint8*)surface->pixels + y * surface->pitch,
+            pixels + y * width * 4,
+            width * 4
+        );
+    }
+
+    SDL_UnlockSurface(surface);
+
+    stbi_image_free(pixels);
+
+    return surface;
 }
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
@@ -52,7 +87,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 #ifndef PLATFORM_WINDOWS
-    SDL_Surface* icon = SDL_LoadBMP(platform_get_resource_path(PROGRAM_ICON_PATH));
+    SDL_Surface* icon = load_image(platform_get_resource_path(PROGRAM_ICON_PATH));
     if (icon)
     {
         SDL_SetWindowIcon(app->window, icon);

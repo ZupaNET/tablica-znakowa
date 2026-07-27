66b39705fa6ddbde921502cc46a204e14f7e4e0e
Rikka <rikka@takanashi.moe>
2026-07-27 22:20:31 +0200

    feat(cmake): use vendored SDL3 libraries only when we do not have system ones

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -4,63 +4,89 @@
 
 include(FetchContent)
 
-set(USE_SYSTEM_SDL OFF)
+set(USE_SYSTEM_SDL3 OFF)
+set(USE_SYSTEM_SDL3_TTF OFF)
+set(USE_SYSTEM_SDL3_NET OFF)
 
 # --------------------------------------
-# Try system SDL3 on Linux
+# Try system libraries on Linux
 # --------------------------------------
 
 if(UNIX AND NOT ANDROID)
 
     find_package(SDL3 CONFIG QUIET)
+    if(SDL3_FOUND)
+        message(STATUS "Using system SDL3")
+        set(USE_SYSTEM_SDL3 ON)
+    endif()
+
     find_package(SDL3_ttf CONFIG QUIET)
+    if(SDL3_ttf_FOUND)
+        message(STATUS "Using system SDL3_ttf")
+        set(USE_SYSTEM_SDL3_TTF ON)
+    endif()
+
     find_package(SDL3_net CONFIG QUIET)
-
-    if(SDL3_FOUND AND SDL3_ttf_FOUND AND SDL3_net_FOUND)
-        message(STATUS "Using system SDL3 libraries")
-        set(USE_SYSTEM_SDL ON)
-
+    if(SDL3_net_FOUND)
+        message(STATUS "Using system SDL3_net")
+        set(USE_SYSTEM_SDL3_NET ON)
     endif()
 
 endif()
 
+
 # --------------------------------------
-# Use FetchContent if unavailable
+# Fetch missing libraries
 # --------------------------------------
 
-if(NOT USE_SYSTEM_SDL)
+# SDL3
+if(NOT USE_SYSTEM_SDL3)
 
-    # SDL3
+    message(STATUS "Fetching SDL3")
+
     set(SDL_TEST_LIBRARY OFF CACHE BOOL "" FORCE)
     set(SDL_TESTS OFF CACHE BOOL "" FORCE)
     set(SDL_EXAMPLES OFF CACHE BOOL "" FORCE)
 
     FetchContent_Declare(
-            SDL3
-            GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-            GIT_TAG main
+        SDL3
+        GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
+        GIT_TAG main
     )
 
     FetchContent_MakeAvailable(SDL3)
 
-    # SDL3_ttf
+endif()
+
+
+# SDL3_ttf
+if(NOT USE_SYSTEM_SDL3_TTF)
+
+    message(STATUS "Fetching SDL3_ttf")
+
     set(SDLTTF_VENDORED ON CACHE BOOL "" FORCE)
     set(FT_DISABLE_INSTALL ON CACHE BOOL "" FORCE)
 
     FetchContent_Declare(
-            SDL3_ttf
-            GIT_REPOSITORY https://github.com/libsdl-org/SDL_ttf.git
-            GIT_TAG main
+        SDL3_ttf
+        GIT_REPOSITORY https://github.com/libsdl-org/SDL_ttf.git
+        GIT_TAG main
     )
 
     FetchContent_MakeAvailable(SDL3_ttf)
 
+endif()
 
-    # SDL3_net
+
+# SDL3_net
+if(NOT USE_SYSTEM_SDL3_NET)
+
+    message(STATUS "Fetching SDL3_net")
+
     FetchContent_Declare(
-            SDL3_net
-            GIT_REPOSITORY https://github.com/libsdl-org/SDL_net.git
-            GIT_TAG main
+        SDL3_net
+        GIT_REPOSITORY https://github.com/libsdl-org/SDL_net.git
+        GIT_TAG main
     )
 
     FetchContent_MakeAvailable(SDL3_net)
